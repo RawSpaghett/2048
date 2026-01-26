14b3539644960e9dbe8d8dd5125dbda361b66518
RawSpaghett <rawspaghetti05@gmail.com>
2026-01-26 18:41:25 -0500

    Playable Prototype
    Can fully be played without UI

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI; //for image
 using TMPro; //text mesh pro
+using System.Collections; //for coroutines
 
 public class Tile : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class Tile : MonoBehaviour
     public TileCell cell{get; private set;}
     //where is this tile currently
     public int number{get; private set;}
+
+    public const float duration = 0.1f; //can be edited in inspector now
+
+    public bool locked{get;set;}
 
     private Image background;
     private TextMeshProUGUI text; //without TMP use "text" instead
@@ -42,5 +47,54 @@ public class Tile : MonoBehaviour
 
         //Sets the position of the tile to the position of the cell
         transform.position = cell.transform.position;
+    }
+    public void MoveTo(TileCell cell){ //identical to spawning except animation
+         if (this.cell != null)
+        {
+            this.cell.Tile = null;
+        }
+
+        //mutally identifys cell to tile and tile to cell
+        this.cell = cell;
+        this.cell.Tile = this;
+
+        //Sets the position of the tile to the position of the cell
+        StartCoroutine(Animate(cell.transform.position,duration, false));
+    }
+
+    public void Merge(TileCell cell)
+    {
+          if (this.cell != null)
+        {
+            this.cell.Tile = null;
+        }
+
+        this.cell = null;
+        cell.Tile.locked = true;
+        StartCoroutine(Animate(cell.transform.position,duration, true));
+    }
+
+    private IEnumerator Animate(Vector3 to,float duration, bool merging) //Animation Coroutine
+    {
+        //timer
+        float elapsed = 0f;
+
+        Vector3 from = transform.position; //current position of tile
+
+        while (elapsed < duration)
+        {
+            transform.position = Vector3.Lerp(from,to,elapsed/duration); 
+            //linear interpolates, essentially slides. Going from A to B
+            elapsed += Time.deltaTime;
+            //Time.deltaTime is how much time has elapsed since the last frame
+            yield return null;
+            //coroutines allow you to essentially suspend something until the next frame
+        }
+        transform.position = to; // to align the tile and avoid slight positioning errors
+
+        if (merging) //if its merging, destroy after moving
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/TileBoard.cs
+++ Assets/Scripts/TileBoard.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //for lists
@@ -5,10 +6,16 @@ using UnityEngine;
 public class TileBoard : MonoBehaviour
 {
     public Tile TilePrefab;
+
     private TileGrid grid;
+
     private List <Tile> tiles;
+
     public TileState[] tileStates;
     //takes in the states we made in the editor as scriptable objects
+
+    private bool waiting; 
+    //for animations
 
     private void Awake()
     {
@@ -34,46 +41,118 @@ public class TileBoard : MonoBehaviour
     private void Update()
     //get user inputs
     {
-        //all of these check in the starting directions
-        if (Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.UpArrow))
+        if(!waiting) //is false, prevents weird movements
         {
-            MoveTiles(Vector2Int.up,0,1,1,1); //checks top down
-        } else if (Input.GetKeyDown(KeyCode.S)|| Input.GetKeyDown(KeyCode.DownArrow)){
-            MoveTiles(Vector2Int.down,0,1,grid.height - 2,-1); //checks bottom up
-        } else if (Input.GetKeyDown(KeyCode.A)|| Input.GetKeyDown(KeyCode.LeftArrow)){
-            MoveTiles(Vector2Int.left,1,1,0,1);//checks left to right
-        } else if (Input.GetKeyDown(KeyCode.D)|| Input.GetKeyDown(KeyCode.RightArrow)){
-            MoveTiles(Vector2Int.right, grid.width-2,-1,0,1); //checks right to left
+            //all of these check in the starting directions
+            if (Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                MoveTiles(Vector2Int.up,0,1,1,1); //checks top down
+            } else if (Input.GetKeyDown(KeyCode.S)|| Input.GetKeyDown(KeyCode.DownArrow)){
+                MoveTiles(Vector2Int.down,0,1,grid.height - 2,-1); //checks bottom up
+            } else if (Input.GetKeyDown(KeyCode.A)|| Input.GetKeyDown(KeyCode.LeftArrow)){
+                MoveTiles(Vector2Int.left,1,1,0,1);//checks left to right
+            } else if (Input.GetKeyDown(KeyCode.D)|| Input.GetKeyDown(KeyCode.RightArrow)){
+                MoveTiles(Vector2Int.right, grid.width-2,-1,0,1); //checks right to left
+            }
         }
+        
     }
     private void MoveTiles(Vector2Int direction,int startX, int incrementX, int startY, int incrementY)
     //IMPORTANT, The direction you read the grid must be in the direction you are going (Moving up, read top down etc)
     //using the Starts and increments prevents redundancy with all directions
     {
+        bool changed = false; //assume board state hasnt changed
+
         for (int x = startX;x >= 0 && x < grid.width; x += incrementX)
         {
-            for (int y = startY; y >= 0 && y < grid.height; startY += incrementY )
+            for (int y = startY; y >= 0 && y < grid.height; y += incrementY )
             {
                 TileCell cell = grid.GetCell(x,y);
 
                 if (cell.occupied)
                 {
-                    MoveTile(cell.tile,direction);
+                    changed |= MoveTile(cell.Tile,direction); //OR equals, once its set to true, it will stay true
                 }
             }
         }
+        if(changed) // if game state has changed
+        {
+            StartCoroutine(WaitForChanges());
+        }
+
     }
-    private void MoveTile(Tile tile, Vector2Int direction)
+    private bool MoveTile(Tile tile, Vector2Int direction)
     {
         TileCell newCell = null;
         TileCell adjacent = grid.GetAdjacentCell(tile.cell,direction);
 
-        while(adjacent != null)
+        while(adjacent != null) //keeps looping till out of bounds or unmergable tile
         {
             if(adjacent.occupied)
             {
-                //Merging
+                if (CanMerge(tile,adjacent.Tile))
+                {
+                    Merge(tile,adjacent.Tile);
+                    return true;
+                }
+                break;
             }
+            newCell = adjacent; //because it moves, the newcell is now the adjacent cell
+            adjacent = grid.GetAdjacentCell(adjacent,direction);
+        }
+        if (newCell != null)
+        {
+            tile.MoveTo(newCell);
+            return true; // did move
+        }
+        return false; //did not move
+    }
+
+    private bool CanMerge(Tile a, Tile b) //not ENTIRELY neccessary
+    {
+        return a.number == b.number && !b.locked; //numbers have to match a and b and B needs to be  unlocked
+    
+    }
+
+    private void Merge(Tile a, Tile b) //delete one of them
+    {
+        tiles.Remove(a); 
+        a.Merge(b.cell);
+
+        int index = Mathf.Clamp(IndexOf(b.state) + 1,0,tileStates.Length - 1); //ensures index always stays in bounds with tilestates, if it exceeds it just uses the last state hence the clamp
+        int number = b.number * 2; //infinite number possibility
+
+        b.SetState(tileStates[index],number); // Not infinite tile states
+    }
+
+    private int IndexOf(TileState State) // checks current state of a tile
+    {
+        for (int i = 0; i < tileStates.Length; i++)
+        {
+            if(State == tileStates[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private IEnumerator WaitForChanges() //handles board state and analyzes changed after every move
+    {
+        waiting = true;
+
+        yield return new WaitForSeconds(Tile.duration);
+
+        waiting = false;
+
+        if(tiles.Count != grid.size) // if available space
+        {
+        CreateTile();
+        }
+        //Check for game over
+
+        foreach (var tile in tiles) //iterates through and unlocks all tiles during update step
+        {
+            tile.locked = false;
         }
     }
 }
